f509bc7b33b746d79451a0cb7df495f5d6ea89da
pancw <pancw3@debian>
2017-08-09 15:44:40 +0800

    move

---
+++ move.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 8;
+	int b = -8;
+
+	printf("%d\n", a >> 1);
+	printf("%d\n", a << 1);
+	
+	
+	printf("%d\n", b >> 1);
+	printf("%d\n", b << 1);
+	
+	return 0;
+}
